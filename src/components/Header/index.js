9a3d43ab2474c83192fee0c79dd034a6614f8bdd
Brendan O'Brien <obrienpbrendan@gmail.com>
2018-03-20 14:29:31 -0600

    change header color & add linkedin image

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
+      background: '#03A9F4',
       marginBottom: '1.45rem',
     }}
   >
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div>
+      <img src={require('../images/linkedin.jpg')} />
+    </div>
+    <div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </div>
   </div>
 )
 
