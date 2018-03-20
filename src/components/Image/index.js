b0b6de8adc8b987f587c613b356178555816a625
Brendan O'Brien <obrienpbrendan@gmail.com>
2018-03-20 15:42:51 -0600

    name, descriptions

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-const Image = ({ source }) => (
-  <div
-    style={{
-      maxWidth: 200
-    }}
-  >
-    <img src={source} />
+const Image = ({ image, name, description }) => { console.log(name); return (
+  <div>
+    <div
+      style={{
+        maxWidth: 200
+      }}
+    >
+      <img src={image} />
+    </div>
+    <h2>{name}</h2>
+    <h5>{description}</h5>
   </div>
-);
+)};
 
 export default Image;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,25 @@ import Link from 'gatsby-link'
 import Image from '../components/Image';
 import { brendan, tom } from '../images';
 
+const nomads = {
+  brendan: {
+    image: brendan,
+    name: "Brendan O'Brien",
+    description: "Yoga Instructor at El Zopilote"
+  },
+  tom: {
+    image: tom,
+    name: "Tom Mayell",
+    description: "Yoga Instructor at El Zopilote"
+  }
+}
+
 const IndexPage = () => (
   <div>
-    {[brendan, tom].map(source => <Image source={source} />)}
+    <h1>Nomads</h1>
+    {Object.values(nomads).map(obj => (
+      <Image {...obj} />
+    ))}
     <div>
       <Link to="/page-2/">Go to page 2</Link>
     </div>
